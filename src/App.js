8c6a78cfb28a008499069f13c6e904e80d08dce4
alicialowe <hohpascal@outlook.com>
2022-07-04 15:24:32 +0100

    updated text on homepage and added an image - this is to test if automatic update works on deployment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Dweeb from "./assets/dweeb.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Hello Dweebs</h1>
+      <p>This site will help you on your life journey as a dweeb.</p>
+      <img src={Dweeb} alt="dweeb" />
     </div>
   );
 }
